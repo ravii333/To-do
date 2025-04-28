86f9375ab6a2d1ed767382cee052a22b01fe9795
Ravi Kumar <ravi.kuamr@oplandtech.com>
2025-04-29 01:43:16 +0530

    A Todo App using js and local-Storage for storing tasks

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+const addTaskButton = document.getElementById('add-task-btn');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo list');
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [] ;
+
+tasks.forEach((task)=> renderTask(task));
+
+addTaskButton.addEventListener('click', ()=>{
+    const taskText = todoInput.value.trim();
+    if(taskText === "") return;
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        complete: false,
+    };
+    tasks.push(newTask);
+    saveTask();
+    todoInput.value = "" ; // clear input
+    console.log(tasks);
+})
+
+function renderTask(task){
+    console.log(task);
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+if(task.completed) li.classList.add("completed");
+li.innerHTML = `
+<span> ${task.text}<span/>
+<button>delete<button/>
+`;
+
+li.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'BUTTON') return;
+    task.completed = !task.completed;
+    li.classList.toggle('complete');
+})
+todoList.appendChild(li);
+}
+
+function saveTask(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+})
